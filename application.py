771f49d00e83538a1747c868e8eeba767e1fe372
ImaiTomoaki <58325267+ImaiTomoaki@users.noreply.github.com>
2020-07-21 19:07:56 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,27 @@
-from flask import Flask
+import os
+import numpy as np
+#import time
+import json
+#import codecs
+import datetime
+import cv2
+#import requests
+#from PIL import Image
+from azure.storage.blob import BlockBlobService
+import base64
+from flask import Flask, request, Response
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World test"
+@app.route('/', methods=['GET', 'POST'])
+def check():
+    try:
+        if request.method != "POST":
+            return 'GET METHOD'
+        else:
+            return 'POST METHOD'
+    except:
+        return 'exception'
+    
+if __name__ == "__main__":
+    app.run()
